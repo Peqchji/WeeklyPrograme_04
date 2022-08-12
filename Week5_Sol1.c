b2ff3046c1339b0ccec16c774837d1dbca69fe3d
Peqchji <jirapat3774@gmail.com>
2022-08-12 19:29:55 +0700

    Create Week5_Sol1.c

---
+++ Week5_Sol1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+int main(){
+    FILE *fp;
+    char string[51];
+    int i;
+    bool check = true;
+    fp = fopen("c:/temp/data.txt", "a"); // ถ้าไม่มีเดี๋ยวมันสร้างให้เอง เพราะโหมด a+
+    printf("Input data string : \n");
+    while (true){
+        scanf("%s", &string);
+        for(i = 0; i < strlen(string); i++){
+            if (strcmp(string, ".") == 0){
+                check = false;
+                break;
+            }
+        }
+        fprintf(fp, "%s\n", string);
+        if(check == false) { break; }
+    }
+    fclose(fp);
+    return 0;
+}
